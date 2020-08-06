d530a5c6bfddce00d7e36af4d228b4e71c0ef725
Scott Lewis <scottcl@outlook.com>
2020-08-05 21:17:05 -0400

    Adding some data for profile

--- BenjiWorldApp/Pages/DogProfile.razor.cs
+++ BenjiWorldApp/Pages/DogProfile.razor.cs
@@ -13,20 +13,44 @@ using System.Threading.Tasks;
 
 namespace BenjiWorldApp.Pages
 {
+    public class GenderModel
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
     public class DogProfileBase : ComponentBase
     {
         public DogProfileBase()
         {
             Model = new DogModel();
             //NotificationService = new NotificationService();
+            Genders = Enum.GetValues(typeof(Gender)).Cast<Gender>().Select(x => new GenderModel() { Name = x.ToString(), Value = (int)x });
         }
 
         public DogModel Model { get; set; }
+        public int GenderValue { get; set; }
 
         [Inject]
         public BenjiAPIClient Client { get; set; }
         [Inject]
         protected NotificationService NotificationService { get; set; }
+        public IEnumerable<GenderModel> Genders; 
+        public void Change(object value)
+        {
+            try
+            {
+                NotificationService.Notify(NotificationSeverity.Info, "Working", value.ToString());
+                Model.Gender = (Gender)value;
+                StateHasChanged();
+            }
+            catch(Exception ex)
+            {
+                NotificationService.Notify(NotificationSeverity.Error, "Failed", ex.Message, 6000);
+            }
+            //var str = value is IEnumerable<object> ? string.Join(", ", (IEnumerable<object>)value) : value;
+
+            //events.Add(DateTime.Now, $"{name} value changed to {str}");
+        }
         protected override async Task OnInitializedAsync()
         {
             var myDog = await Client.GetDog(1);
@@ -35,6 +59,7 @@ namespace BenjiWorldApp.Pages
             Model.AdoptedDate = myDog.AdoptedDate;
             Model.Birthdate = myDog.Birthdate;
             Model.Gender = myDog.Gender;
+            GenderValue = (int)myDog.Gender;
             Model.Created = myDog.Created;
             Model.Modified = myDog.Modified;
             Model.Deleted = myDog.Deleted;
